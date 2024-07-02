83655abadd1c3df2b0bf356e0a00fd9ca60ff5ea
Vadym Bakuma <bakuma.2001@gmail.com>
2024-07-02 21:29:21 +0200

    make build

---
+++ index.5ed43a30.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".tree span").forEach(function(e){e.addEventListener("click",function(n){n.stopPropagation();var t=e.nextElementSibling;t&&"UL"===t.tagName&&(t.style.display="none"===t.style.display?"block":"none")})});
+//# sourceMappingURL=index.5ed43a30.js.map
